e67068a01310f4052ad8c9bb4547ce515170006d
Noss <noss@Prometheus>
2023-08-12 01:34:08 -0500

    Updated Bufferline

--- lua/svim/bufferline.lua
+++ lua/svim/bufferline.lua
@@ -16,6 +16,7 @@ function M.config()
       right_mouse_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
       offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
       separator_style = "thin",            -- | "thick" | "thin" | { 'any', 'any' },
+      buffer_close_icon = "",
     },
     highlights = {
       fill = {
