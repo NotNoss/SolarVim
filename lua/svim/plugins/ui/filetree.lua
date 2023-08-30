a0c1634d7624ef101abfb98519f04b10680e03ac
NotNoss <130167541+NotNoss@users.noreply.github.com>
2023-08-29 19:28:41 -0500

    Added Organization to Plugins Folder

---
+++ lua/svim/plugins/ui/filetree.lua
@@ -0,0 +1,58 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.opt.termguicolors = true
+
+return {
+  "nvim-tree/nvim-tree.lua",
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    require("nvim-tree").setup({
+      update_focused_file = {
+        enable = true,
+        update_cwd = true,
+      },
+      renderer = {
+        icons = {
+          glyphs = {
+            default = "",
+            symlink = "",
+            folder = {
+              arrow_open = "",
+              arrow_closed = "",
+              default = "",
+              open = "",
+              empty = "",
+              empty_open = "",
+              symlink = "",
+              symlink_open = "",
+            },
+            git = {
+              unstaged = "",
+              staged = "S",
+              unmerged = "",
+              renamed = "➜",
+              untracked = "U",
+              deleted = "",
+              ignored = "◌",
+            },
+          },
+        },
+      },
+      diagnostics = {
+        enable = true,
+        show_on_dirs = true,
+        icons = {
+          hint = "󰌵",
+          info = "",
+          warning = "",
+          error = "",
+        },
+      },
+      view = {
+        width = 30,
+        side = "left",
+      },
+    })
+  end
+}
